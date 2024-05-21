cafd65d3a53276432399164cf80d34030cf920b9
Komal <mishrkomal@gmail.com>
2024-05-22 00:25:07 +0530

    updated pub key path

--- variables.tf
+++ variables.tf
@@ -13,5 +13,5 @@ variable "AMI" {
 }
 
 variable "PUBLIC_KEY_PATH" {
-    default = "/Users/praveensingampalli/Documents/BOOTCAMP2_FINAL/Terraform_learning_demo/Terraform_learning_demo/oregon-region-key-pair.pub"
+    default = "/E:/DevOpsSingampalli/Terraformcode/Terraform_wc/ohio-region-key-pair"
 }
